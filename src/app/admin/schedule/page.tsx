8b77a5ddcd2fc4b6118dbc148d16aa8c9cf7e2a4
Іван Шевцов <vanyashevcov@gmail.com>
2025-08-11 20:57:20 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SlotClone data-st

--- src/app/admin/schedule/page.tsx
+++ src/app/admin/schedule/page.tsx
@@ -5,11 +5,8 @@ import BellScheduleManager from "@/components/admin/bell-schedule-manager";
 
 export default function SchedulePage() {
      return (
-        <div className="flex-1 pt-6 flex flex-col">
-             <div className="flex items-center justify-between space-y-2 px-8">
-                <h2 className="text-3xl font-bold tracking-tight">Розклад дзвінків</h2>
-            </div>
-            <div className="flex-1 pt-2">
+        <div className="flex-1 flex flex-col">
+            <div className="flex-1">
                 <BellScheduleManager />
             </div>
         </div>
